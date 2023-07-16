83481e2fc6f1fe5557688384a5e06df1cc0c4feb
Suyog Powar <suyogpowar0208@gmail.com>
2023-07-16 21:09:24 +0200

    Setting up basic spring boot maven project

---
+++ src/test/java/com/test/database/postgres/postgrestestcontainersdemo/PostgresTestContainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.database.postgres.postgrestestcontainersdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresTestContainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
