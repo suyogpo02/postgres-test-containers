3fbdb1a1d9bd867d22545e822e2004a25bd44615
Suyog Powar <suyogpowar0208@gmail.com>
2023-07-23 18:11:23 +0200

    initial setup - spring boot project - sql script to create the table using flyway dependency

--- src/test/java/com/test/database/postgres/postgrestestcontainersdemo/PostgresTestContainersDemoApplicationTests.java
+++ src/test/java/com/test/database/postgres/postgrestestcontainersdemo/PostgresTestContainersDemoApplicationTests.java
@@ -1,9 +1,18 @@
 package com.test.database.postgres.postgrestestcontainersdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ImportAutoConfiguration({
+		DataSourceAutoConfiguration.class,
+		FlywayAutoConfiguration.class,
+		JdbcTemplateAutoConfiguration.class
+})
 class PostgresTestContainersDemoApplicationTests {
 
 	@Test
